32433b5e702db89b6635c2e93888fa6a997fa5a7
Maxime Francois <kezho@me.com>
2015-01-06 17:27:36 +1100

    chore(widget): Add facade for the widgets

--- app/config/app.php
+++ app/config/app.php
@@ -189,6 +189,7 @@ return array(
 		'URL'               => 'Illuminate\Support\Facades\URL',
 		'Validator'         => 'Illuminate\Support\Facades\Validator',
 		'View'              => 'Illuminate\Support\Facades\View',
+        'Widget'            => 'Distilleries\Expendable\Facades\Widget',
 
 	),
 
